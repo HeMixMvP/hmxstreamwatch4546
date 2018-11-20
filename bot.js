0ea2700ed49b2ceb8fe45dbfcc0fa06cc2694fe8
MDX15 <45093539+MDX15@users.noreply.github.com>
2018-11-20 11:18:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "h";
 var adminprefix = 'h'
 
-const developers = ["472413769700474901","id"]
+const developers = ["445973548117524491","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
